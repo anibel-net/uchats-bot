d8be7c057f80f9796eb7e5986f1240dd7891e76b
Aliaksiej Razumaŭ <belarusaed@gmail.com>
2021-10-11 15:09:08 +0300

    Heartbeat

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 import os
 
 from dotenv import load_dotenv
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from pyrogram import Client
+import requests
 
 load_dotenv()
 
 if __name__ == '__main__':
     app = Client(':memory:', os.environ.get('API_ID'), os.environ.get('API_HASH'),
                  bot_token=os.environ.get('BOT_TOKEN'), plugins=dict(root='handlers'))
+    scheduler = AsyncIOScheduler()
+    scheduler.add_job(lambda: requests.get(os.environ.get('HEARTBEAT_URL')), 'interval', minutes=1)
+    scheduler.start()
     app.run()
