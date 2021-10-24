48b620592589a0aaafc14873e4fc6c17c7e4f3e9
Aliaksiej Razumaŭ <belarusaed@gmail.com>
2021-10-24 17:52:08 +0300

    uchats-only noy single-chat, chat can be set in .env

--- handlers/08-single_chat.py
+++ handlers/08-single_chat.py
@@ -7,7 +7,7 @@ from pyrogram.types import Message
 
 @Client.on_message(group=708)
 def on_message(_: Client, message: Message):
-    if message.chat.id != -1001322065959 and os.environ.get('DEVMODE').upper() != 'TRUE':
+    if message.chat.id != os.environ.get('CHAT_ID') and os.environ.get('DEVMODE').upper() != 'TRUE':
         message.chat.leave()
         # <editor-fold defaultstate="collapsed" desc="logging">
         logger.info(f'[{message.chat.id} ({message.message_id})] Left chat.')
