e03f87042c2f7487dbee026bf017b7d826395557
Aliaksiej Razumaŭ <belarusaed@gmail.com>
2021-11-21 03:17:03 +0300

    Now deleting banned channels links

--- handlers/07-ban_channels.py
+++ handlers/07-ban_channels.py
@@ -5,6 +5,7 @@ from loguru import logger
 from pyrogram import Client, filters
 from pyrogram.errors import PeerIdInvalid, UsernameNotOccupied, FloodWait, UsernameInvalid
 from pyrogram.types import Message
+from pyrogram.raw.types import InputPeerChannel
 
 from db.ChatData import ChatData
 from functions import is_admin
@@ -98,6 +99,39 @@ async def on_forward(client: Client, message: Message):
     logger.info(f'[{message.chat.id} ({message.message_id})] Done.')
     # </editor-fold>
     return
+
+
+@Client.on_message(filters.regex(r'(?P<url>t.me/[^\s]+)'), group=107)
+async def on_link(client: Client, message: Message):
+    if message.from_user:
+        # <editor-fold defaultstate="collapsed" desc="logging">
+        logger.info(f'[{message.chat.id} ({message.message_id})] Received message'
+                    f'from @{message.from_user.username} ({message.from_user.id}): {message.text}')
+        # </editor-fold>
+        if await is_admin(client, message.chat.id, message.from_user.id):
+            # <editor-fold defaultstate="collapsed" desc="logging">
+            logger.info(f'[{message.chat.id} ({message.message_id})] user @{message.from_user.username} '
+                        f'({message.from_user.id}) is administrator, ignoring.')
+            # </editor-fold>
+            return
+    if message.sender_chat:
+        # <editor-fold defaultstate="collapsed" desc="logging">
+        logger.info(f'[{message.chat.id} ({message.message_id})] Received message'
+                    f'from @{message.sender_chat.username} ({message.sender_chat.id}): {message.text}. Ignoring.')
+        # </editor-fold>
+        return
+    # <editor-fold defaultstate="collapsed" desc="logging">
+    logger.info(f'[{message.chat.id} ({message.message_id})] Initialization chat_data...')
+    # </editor-fold>
+    chat_data = ChatData()
+    await chat_data.init(message.chat.id)
+    # <editor-fold defaultstate="collapsed" desc="logging">
+    logger.info(f'[{message.chat.id} ({message.message_id})] initialized.')
+    # </editor-fold>
+    for match in message.matches:
+        chat = await client.get_chat(match.group('url').split('/')[1])
+        if chat.id in chat_data.banned_channels:
+            await message.delete()
 
 
 @Client.on_message(filters.command('ban channel'), group=207)
