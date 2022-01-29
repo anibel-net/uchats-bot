775f4eff94615676646c99ab2102df2b7aefd3cc
Aliaksiej Razumaŭ <belarusaed@gmail.com>
2022-01-29 23:46:08 +0300

    Fixes

--- handlers/09-automute_not_members.py
+++ handlers/09-automute_not_members.py
@@ -3,15 +3,18 @@ from pyrogram.errors import UserNotParticipant
 from pyrogram.types import Message, ChatPermissions
 
 from db.ChatData import ChatData
-from functions import admin_filter
+from functions import is_admin
 
 
-@Client.on_message(~filters.service & ~admin_filter, group=709)
+@Client.on_message(~filters.service, group=709)
 async def on_any_message(client: Client, message: Message):
     chat_data = ChatData()
     await chat_data.init(message.chat.id)
-    if message.sender_chat.id in chat_data.whitelisted_channels:
-        return
+    if message.sender_chat:
+        if message.sender_chat.id in chat_data.whitelisted_channels or await is_admin(None, None, message):
+            return
+        else:
+            return message.delete()
     try:
         await client.get_chat_member(message.chat.id, message.from_user.id)
     except UserNotParticipant:
